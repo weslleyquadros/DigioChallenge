42decb948f502f26d0750001f85f4c8391117a20
Weslley Quadros <31475603+weslleyquadros@users.noreply.github.com>
2024-08-20 18:06:49 -0300

    Remove files and directories that are now ignored by .gitignore

--- Pods/Target Support Files/Pods-DigioChallenge-DigioChallengeUITests/Pods-DigioChallenge-DigioChallengeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigioChallenge_DigioChallengeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DigioChallenge_DigioChallengeUITests
-@end
--- Pods/Target Support Files/Pods-DigioChallengeTests/Pods-DigioChallengeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigioChallengeTests : NSObject
-@end
-@implementation PodsDummy_Pods_DigioChallengeTests
-@end
